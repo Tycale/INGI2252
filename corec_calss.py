c759057315d90adf8b1b6a7aebd97e9cb284208e
Gérondal Thibault <gthibault5@gmail.com>
2014-12-19 21:01:24 +0100

    Files

--- corec_calss.py
+++ corec_calss.py
@@ -6,10 +6,10 @@ class_regex = re.compile(r'\[(.*?)(, (.*))?\]')
 f = open('pylint_reddit_report.parse', 'r')
 
 # (.*?):(\d+): \[(.*?)\] (.*)
-dict_file = {}
+dict_class = {}
 code_set  = set()
-file_set = set()
-#exclude_code = ['C0202', 'C0203', 'C0321', 'C0322', 'C0324', 'E0101', 'E0611', 'E0711', 'F0401', 'W0102', 'W0105', 'W0106', 'W0122', 'W0231', 'W0311', 'W0403', 'W0404', 'W0701', 'W0702', 'W0703']
+class_set = set()
+#exclude_code = ['C0203', 'C0321', 'C0322', 'C0324', 'E0101', 'F0401', 'W0102', 'W0105', 'W0106', 'W0122', 'W0231', 'W0311', 'W0701', 'W0702', 'W0703']
 exclude_code = []
 # 'W0601' to check 
 
@@ -27,19 +27,20 @@ for i, line in enumerate(f.readlines()):
         code = ma.group(1)
         classe = ma.group(3)
         if classe:
-            if classe[0].isupper():
-                if len(classe.split('.')) > 1:
-                    classe = classe.split('.')[0]
+            #print classe
+            if not classe.split('.')[-1].isupper():
+            #if 1==1:
+                classe = classe.split('.')[1]
+                if not classe in dict_class:
+                    dict_class[classe] = set()
+
+                dict_class[classe].add(code)
+                code_set.add(code)
+                class_set.add(classe)
             else:
                 classe = None
-                if not filename in dict_file:
-                    dict_file[filename] = set()
-
-                dict_file[filename].add(code)
-                code_set.add(code)
-                file_set.add(filename)
-
         message = m.group(1)
+        
     except :
         pass
 
@@ -47,16 +48,16 @@ sorted_code = list(code_set)
 sorted_code.sort()
 #print sorted_code
 
-sorted_file = list(file_set)
-sorted_file.sort()
-#print sorted_file
+sorted_class = list(class_set)
+sorted_class.sort()
+#print sorted_class
 
 matrix = []
 
 for code in sorted_code:
     temp = []
-    for fn in sorted_file:
-        if code in dict_file[fn]:
+    for cl in sorted_class:
+        if code in dict_class[cl]:
             temp.append(True)
         else:
             temp.append(False)
@@ -80,6 +81,8 @@ def D(c1, c2):
     return float(sum(res1)) / float(sum(res2))
 
 
+
+show_code = list(sorted_code)
 new_matrix = []
 
 for code1 in sorted_code:
@@ -93,12 +96,12 @@ for code1 in sorted_code:
 total_code = len(sorted_code)
 for code in sorted_code:
     pc = 0
-    for k,v in dict_file.iteritems():
+    for k,v in dict_class.iteritems():
         if code in v:
             pc += 1
     pc = float(pc) / float(total_code)
     if pc > 0.9: 
-        exclude_code.append(code)
+    	    exclude_code.append(code)
 
 i = 0
 for line in new_matrix:
@@ -108,9 +111,13 @@ for line in new_matrix:
 
 print 'Excluded codes : ', exclude_code
 
-show_code = list(sorted_code)
-for ec in exclude_code:
-    show_code.remove(ec)
+
+for ec in set(exclude_code):
+    try:
+        show_code.remove(ec)
+    except:
+        pass
+
 
 tmpl = Template(u'''\
 <!DOCTYPE html>
@@ -140,9 +147,9 @@ tmpl = Template(u'''\
         {% for item2 in item %}
 
             {% if sorted_code[(loop.index)-1] not in exclude_code %}
-
                 <td style="background-color: rgb({{ '%d' | format(255*item2)  }}, {{ '%d' | format(255 * (100- (item2*100)) / 100) }}, 0);">
-                    {% if item2 < 0.7 and item2 != 0 %}
+                    
+                    {% if item2 < 0.9 and item2 != 0 %}
                         {{ '%.2f' | format(item2) }}
                     {% endif %}
                     
